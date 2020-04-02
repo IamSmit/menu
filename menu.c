a970dc16c525bb4e9e877d3d000d254bd6cd722d
Smit Patel <53096762+IamSmit@users.noreply.github.com>
2020-04-02 13:25:38 +0530

    Update menu.c

--- menu.c
+++ menu.c
@@ -8,33 +8,45 @@ int main()
     printf("\t\tMAIN MENU\n");
     printf("1.All Product Records\n2.Selling Records\n3.Purchase Records\n4.Return Records\n5.Exit\n");
     printf("Enter the particular number for the particular menu option to be operated.");
-    int num;
+    int num,x;
     scanf("%d",&num);
     switch (num)
     {
     case 1:
         printf("You Selected \"All Product Records\"\n\"PRESS 0 TO GO TO MAIN MENU\"");
+        scanf("%d",&x);
+        if(x==0)
+        goto mainmenu;
         break;
     
     case 2:
         printf("You Selected \"Selling Records\"\n\"PRESS 0 TO GO TO MAIN MENU\"");
+        scanf("%d",&x);
+        if(x==0)
+        goto mainmenu;
         break;
 
     case 3:
         printf("You Selected \"Purchase Records\"\n\"PRESS 0 TO GO TO MAIN MENU\"");
+        scanf("%d",&x);
+        if(x==0)
+        goto mainmenu;
         break;
 
     case 4:
         printf("You Selected \"Return Records\"\n\"PRESS 0 TO GO TO MAIN MENU\"");
+        scanf("%d",&x);
+        if(x==0)
+        goto mainmenu;
         break;
 
     case 5:
         printf("You Selected \"Exit from Menu\"\n\"PRESS 0 TO GO TO MAIN MENU\"");
-        break;
-
-    case 0:
+        scanf("%d",&x);
+        if(x==0)
         goto mainmenu;
         break;
+
     default:
         printf("\"Enter the number from 0 to 5.\"");
         break;
